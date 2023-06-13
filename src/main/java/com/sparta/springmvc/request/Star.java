ae60578cf5b53d018bbf59b57165d016e2d8ef6b
chae0 <limchae098@gmail.com>
2023-06-13 12:40:34 +0900

    how to send data from Client to Server, ModelAttribute

---
+++ src/main/java/com/sparta/springmvc/request/Star.java
@@ -0,0 +1,15 @@
+package com.sparta.springmvc.request;
+
+import lombok.Getter;
+
+@Getter
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
